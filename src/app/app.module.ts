a393f7a9ca7bf1dde99fbf14e57512ccc154cdab
Ion <prc_ion@outlook.com>
2020-11-03 16:59:48 +0200

    implement login & register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {Register} from 'ts-node';
+import {UserComponent} from './components/user/user.component';
+import {FormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {NotificationModule} from './notification/notification.module';
+import {NotificationService} from './notification/notification.service';
+import {AuthenticationService} from './service/authentication.service';
+import {UserService} from './service/user.service';
+import {AuthGuard} from './guard/auth.guard';
+import {AuthInterceptor} from './interceptors/auth.interceptor';
+import {RegisterComponent} from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NotificationModule,
   ],
-  providers: [],
+  providers: [
+    NotificationService,
+    AuthenticationService,
+    UserService,
+    AuthGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthenticationService} from '../service/authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationSevice: AuthenticationService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.url.includes(`${this.authenticationSevice.host}/auth/signin`)) {
+      return next.handle(request);
+    }
+
+    if (request.url.includes(`${this.authenticationSevice.host}/auth/signup`)) {
+      return next.handle(request);
+    }
+
+    if (request.url.includes(`${this.authenticationSevice.host}/user/resetpassword`)) {
+      return next.handle(request);
+    }
+
+    this.authenticationSevice.loadToken();
+    const token = this.authenticationSevice.getToken();
+
+    return next.handle(request.clone({setHeaders: {Authorization: `Bearer ${token}`}}));
+  }
+}
